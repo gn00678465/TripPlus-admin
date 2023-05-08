9842a228c61de3ca6e55f05a3fdf43eb87956559
Madao <gn00678465@gmail.com>
2023-05-08 15:22:06 +0800

    feat: 新增 Table 無資料時顯示

--- src/pages/admin/projects/components/Table/index.tsx
+++ src/pages/admin/projects/components/Table/index.tsx
@@ -14,8 +14,11 @@ import {
   Th,
   Td,
   TableContainer,
-  TableContainerProps
+  TableContainerProps,
+  Text,
+  Icon
 } from '@chakra-ui/react';
+import { SlDrawer } from 'react-icons/sl';
 
 export type DataTableProps<T extends object> = {
   data: T[];
@@ -75,22 +78,39 @@ export default function DataTable<T extends object>({
           ))}
         </Thead>
         <Tbody>
-          {table.getRowModel().rows.map((row) => (
-            <Tr key={row.id}>
-              {row.getVisibleCells().map((cell) => {
-                const meta: any = cell.column.columnDef.meta;
-                return (
-                  <Td
-                    key={cell.id}
-                    isNumeric={meta?.isNumeric}
-                    color="gray.500"
-                  >
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </Td>
-                );
-              })}
+          {table.getRowModel().rows.length ? (
+            table.getRowModel().rows.map((row) => (
+              <Tr key={row.id}>
+                {row.getVisibleCells().map((cell) => {
+                  const meta: any = cell.column.columnDef.meta;
+                  return (
+                    <Td
+                      key={cell.id}
+                      isNumeric={meta?.isNumeric}
+                      color="gray.500"
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </Td>
+                  );
+                })}
+              </Tr>
+            ))
+          ) : (
+            <Tr>
+              <Td
+                colSpan={columns.length}
+                textAlign="center"
+                color="gray.300"
+                py="12"
+              >
+                <Icon as={SlDrawer} boxSize={16} />
+                <Text mt="1">No Data</Text>
+              </Td>
             </Tr>
-          ))}
+          )}
         </Tbody>
       </Table>
     </TableContainer>

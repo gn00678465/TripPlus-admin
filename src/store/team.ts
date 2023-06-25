dec494eceb601a041d8ee7815c53f7b66c6d3595
YuHan Huang <taiwansmile@gmail.com>
2023-06-26 00:59:32 +0800

    fix: 修正 teamId 儲存方式

--- src/store/team.ts
+++ src/store/team.ts
@@ -1,11 +1,20 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface TeamState {
   teamId: string;
   setTeamId: (id: string) => void;
 }
 
-export const useTeamStore = create<TeamState>()((set) => ({
-  teamId: '',
-  setTeamId: (id) => set({ teamId: id })
-}));
+export const useTeamStore = create<TeamState>()(
+  persist(
+    (set, get) => ({
+      teamId: '',
+      setTeamId: (id) => set({ teamId: id })
+    }),
+    {
+      name: 'teamId',
+      storage: createJSONStorage(() => localStorage)
+    }
+  )
+);

62c501acdd73d6453d5ab29b31c9a0f7b0b972ee
Madao <gn00678465@gmail.com>
2023-06-28 21:57:35 +0800

    feat: 更換 ckEditor 編輯器 class to document

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 import { MyUploadAdapterPlugin } from './UploadAdapter';
 
 interface CKeditorProps {
@@ -18,13 +19,13 @@ export default function CKeditor({
 }: CKeditorProps) {
   const editorRef = useRef<{
     CKEditor: typeof CKEditor;
-    ClassicEditor: typeof ClassicEditor;
+    DecoupledEditor: typeof DecoupledEditor;
   }>();
 
   useEffect(() => {
     editorRef.current = {
       CKEditor: require('@ckeditor/ckeditor5-react').CKEditor,
-      ClassicEditor: require('@ckeditor/ckeditor5-build-classic')
+      DecoupledEditor: require('@ckeditor/ckeditor5-build-decoupled-document')
     };
   }, []);
 
@@ -34,29 +35,37 @@ export default function CKeditor({
         <div>
           <CKEditor
             id={name}
-            editor={ClassicEditor}
+            editor={DecoupledEditor}
             data={value}
             config={{
-              toolbar: [
-                'heading',
-                '|',
-                'bold',
-                'italic',
-                'blockQuote',
-                'link',
-                'numberedList',
-                'bulletedList',
-                'imageUpload',
-                'insertTable',
-                'tableColumn',
-                'tableRow',
-                'mergeTableCells',
-                'mediaEmbed',
-                '|',
-                'undo',
-                'redo'
-              ],
+              // toolbar: [
+              //   'heading',
+              //   '|',
+              //   'bold',
+              //   'italic',
+              //   'blockQuote',
+              //   'link',
+              //   'numberedList',
+              //   'bulletedList',
+              //   'imageUpload',
+              //   'insertTable',
+              //   'tableColumn',
+              //   'tableRow',
+              //   'mergeTableCells',
+              //   'mediaEmbed',
+              //   '|',
+              //   'undo',
+              //   'redo'
+              // ],
               extraPlugins: [MyUploadAdapterPlugin]
+            }}
+            onReady={(editor) => {
+              editor.ui
+                .getEditableElement()
+                ?.parentElement?.insertBefore(
+                  editor.ui.view.toolbar.element as HTMLElement,
+                  editor.ui.getEditableElement() as HTMLElement
+                );
             }}
             onChange={(event: any, editor: any) => {
               const data = editor.getData();
